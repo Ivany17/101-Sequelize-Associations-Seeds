b3373d378e24a20f31a1f9ca51a633556a910c46
Ivan Yaremko <yaremich98@gmail.com>
2023-04-04 09:03:51 +0300

    (101) Sequelize. Associations. Seeds

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = require('./router');
+
+const app = express();
+app.use(express.json());
+app.use('/api', router);
+
+app.use((err, req, res, next)=> {
+    res.status(500).send({
+        errors: [{message: err.message}]
+    })
+})
+
+module.exports = app;
---
+++ controllers/user.controller.js
@@ -0,0 +1,55 @@
+const { User } = require('../models');
+
+module.exports.createUser = async (req, res, next) => {
+    try {
+        const {body} = req;
+        const createdUser = await User.create(body);
+        res.status(201).send({data:createdUser});
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.findAll({
+            where: {
+                firstName:'Elon'
+            },
+            attributes: {
+                exclude: ['password']
+            }
+        });
+        res.status(200).send({data:users});
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports.updateUser = async (req, res, next) => {
+    try {
+        const {body, params:{id}} = req;
+        const [rows, [updateUser]] = await User.update(body, {
+            where: {id},
+            returning: true //['first_name', 'last_name', 'email']
+        });
+        updateUser.password = undefined;
+        res.status(200).send({data:updateUser});
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports.updateUserInstance = async (req, res, next) => {
+    try {
+        const {body, params:{id}} = req;
+        //const userInstance = await User.findByPk(id);
+        const updatedUser = await userInstance.update(body, {
+            returning: true
+        });
+        updatedUser.password = undefined;
+        res.status(200).send({data:updatedUser});
+    } catch (error) {
+        next(error)
+    }
+}
